2a9a88fc91f0a0c22439de50b1ffd38308ab9c6e
Grady997 <GradyA@wangt.onmicrosoft.com>
2021-01-29 08:05:32 +0800

    old

--- docker/jd-docker.sh
+++ docker/jd-docker.sh
@@ -1,11 +1,46 @@
-#!/bin/bash
-set -e
+#!/bin/sh
+#
+# Copyright (C) 2020 老竭力
+#
+# This is free software, licensed under the GNU General Public License v3.
+# See /LICENSE for more information.
+#
+clear
+
+echo "
+     ██╗██████╗     ██████╗  ██████╗  ██████╗██╗  ██╗███████╗██████╗ 
+     ██║██╔══██╗    ██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+     ██║██║  ██║    ██║  ██║██║   ██║██║     █████╔╝ █████╗  ██████╔╝
+██   ██║██║  ██║    ██║  ██║██║   ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+╚█████╔╝██████╔╝    ██████╔╝╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║
+ ╚════╝ ╚═════╝     ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                                     
+                     ==== Create by 老竭力 ====                     
+              本脚本将会添加作者的助力码，感谢你的支持！             
+"
+DOCKER_IMG_NAME="evinedeng/jd"
 JD_PATH=""
 SHELL_FOLDER=$(pwd)
 CONTAINER_NAME=""
+CONFIG_PATH=""
+LOG_PATH=""
+GIT_SOURCE="gitee"
+
+HAS_IMAGE=false
+PULL_IMAGE=true
+
+HAS_CONTAINER=false
+DEL_CONTAINER=true
+INSTALL_WATCH=false
+
+TEST_BEAN_CHAGE=false
 
 log() {
     echo -e "\e[32m$1 \e[0m\n"
+}
+
+inp() {
+    echo -e "\e[33m$1 \e[0m\n"
 }
 
 warn() {
@@ -14,7 +49,7 @@ warn() {
 
 cancelrun() {
     if [ $# -gt 0 ]; then
-        echo -e "\033[31m $1 \033[0m"
+        echo     "\033[31m $1 \033[0m"
     fi
     exit 1
 }
@@ -34,100 +69,122 @@ docker_install() {
             echo "安装docker环境..."
             curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
             echo "安装docker环境...安装完成!"
-            systemctl restart docker
+            systemctl enable docker
+            systemctl start docker
         fi
     fi
 }
 
 docker_install
-
-echo -n -e "\e[33m请输入配置文件保存的绝对路径,直接回车为当前目录:\e[0m"
+warn "注意如果你什么都不清楚，建议所有选项都直接回车，使用默认选择！！！"
+#配置文件目录
+echo -n -e "\e[33m一.请输入配置文件保存的绝对路径,直接回车为当前目录:\e[0m"
 read jd_path
 JD_PATH=$jd_path
 if [ -z "$jd_path" ]; then
     JD_PATH=$SHELL_FOLDER
 fi
+CONFIG_PATH=$JD_PATH/jd_docker/config
+LOG_PATH=$JD_PATH/jd_docker/log
 
-config_path=$JD_PATH/jd_docker/config
-log_path=$JD_PATH/jd_docker/log
-log "本脚本将会添加作者（老竭力）的助力码，感谢你的支持！"
+#选择镜像
+#inp "二.请选择容器中脚本更新源：\n1) gitee[默认]\n2) github"
+#echo -n -e "\e[33m输入您的选择->\e[0m"
+#read source
+#if [ "$source" = "2" ]; then
+#    GIT_SOURCE="github"
+#fi
+
+#检测镜像是否存在
+if [ ! -z "$(docker images -q $DOCKER_IMG_NAME:$GIT_SOURCE 2> /dev/null)" ]; then
+    HAS_IMAGE=true
+    inp "检测到先前已经存在的镜像，是否拉取最新的镜像：\n1) 是[默认]\n2) 不需要"
+    echo -n -e "\e[33m输入您的选择->\e[0m"
+    read update
+    if [ "$update" = "2" ]; then
+        PULL_IMAGE=false
+    fi
+fi
+
+#检测容器是否存在
+check_container_name() {
+    if [ ! -z "$(docker ps -a | grep $CONTAINER_NAME 2> /dev/null)" ]; then
+        HAS_CONTAINER=true
+        inp "检测到先前已经存在的容器，是否删除先前的容器：\n1) 是[默认]\n2) 不要"
+        echo -n -e "\e[33m输入您的选择->\e[0m"
+        read update
+        if [ "$update" = "2" ]; then
+            PULL_IMAGE=false
+            inp "您选择了不要删除之前的容器，需要重新输入容器名称"
+            input_container_name
+        fi
+    fi
+}
+
+#容器名称
+input_container_name() {
+    echo -n -e "\e[33m三.请输入要创建的Docker容器名称[默认为：jd]->\e[0m"
+    read container_name
+    if [ -z "$container_name" ]; then
+        CONTAINER_NAME="jd"
+    else
+        CONTAINER_NAME=$container_name
+    fi
+    check_container_name
+}
+input_container_name
+
+#是否安装WatchTower
+inp "5.是否安装containrrr/watchtower自动更新Docker容器：\n1) 安装\n2) 不安装[默认]"
+echo -n -e "\e[33m输入您的选择->\e[0m"
+read watchtower
+if [ "$watchtower" = "1" ]; then
+    INSTALL_WATCH=true
+fi
+
+
+#配置已经创建完成，开始执行
+
 log "1.开始创建配置文件目录"
-mkdir -p $config_path
-mkdir -p $log_path
+mkdir -p $CONFIG_PATH
+mkdir -p $LOG_PATH
 
-log "2.开始下载配置文件"
-wget -q --no-check-certificate https://raw.githubusercontent.com/Grady997/jd_script/main/sample/config.sh.sample -O $config_path/config.sh
-if [ $? -ne 0 ]; then
-    cancelrun "下载配置文件出错请重试"
+
+if [ $HAS_IMAGE = true ] && [ $PULL_IMAGE = true ]; then
+    log "2.1.开始拉取最新的镜像"
+    docker pull $DOCKER_IMG_NAME:$GIT_SOURCE
 fi
 
-#添加脚本作者助力码
-
-
-wget -q --no-check-certificate https://raw.githubusercontent.com/Grady997/jd_script/main/sample/crontab.list.sample -O $config_path/crontab.list
-if [ $? -ne 0 ]; then
-    cancelrun "下载配置文件出错请重试"
-fi
-
-echo -n -e "配置文件config.sh已经下载到$config_path目录下，你可以手动去修改config.sh中的配置，也可以在这里输入第一个cookie:\n[Cookie的具体形式（只有pt_key字段和pt_pin字段，没有其他字段）：pt_key=xxxxxxxxxx;pt_pin=xxxx;]\n\e[33mCookie1->\e[0m"
-read cookie
-
-if [ -z "$cookie" ]; then
-    warn "您没有输入cookie,请手动去$config_path/config.sh中修改"
-else
-    sed -i 's/Cookie1=""/Cookie1="'$cookie'"/g' $config_path/config.sh
-fi
-
-echo -n -e "请输入server酱的PUSH_KEY\n[ServerChan，教程：http://sc.ftqq.com/3.version]\n\e[33mPUSH_KEY->\e[0m"
-read pushKey
-
-if [ -z "$pushKey" ]; then
-    warn "您没有输入PUSH_KEY,所以不会接收推送消息，如有需要可以手动去$config_path/config.sh中修改。\n也可以使用其他推送如：BARK\Telegram\钉钉\iGot聚合推送\酷推\Push Plus等，config.sh中有详细说明"
-else
-    sed -i 's/export PUSH_KEY=""/export PUSH_KEY="'$pushKey'"/g' $config_path/config.sh
-fi
-
-echo -n -e "\e[33m请输入要创建的 docker container 的名称[默认为：jd-script]->\e[0m"
-read container_name
-if [ -z "$container_name" ]; then
-    CONTAINER_NAME="jd-script"
-else
-    CONTAINER_NAME=$container_name
+if [ $HAS_CONTAINER = true ] && [ $DEL_CONTAINER = true ]; then
+    log "2.2.删除先前的容器"
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
 fi
 
 log "3.开始创建容器并执行"
-
 docker run -dit \
-    -v $config_path:/jd/config \
-    -v $log_path:/jd/log \
-    -p 5678:5678 \
+    -v $CONFIG_PATH:/jd/config \
+    -v $LOG_PATH:/jd/log \
     --name $CONTAINER_NAME \
     --hostname jd \
     --restart always \
     --network host \
-    evinedeng/jd:gitee
+    $DOCKER_IMG_NAME:$GIT_SOURCE
 
-log "4.是否安装containrrr/watchtower自动更新Docker容器：\n1) 安装[默认]\n2) 不安装"
-echo -n -e "\e[33m输入您的选择->\e[0m"
-read watchtower
-if [ -z "$watchtower" ] || [ "$watchtower" == "1" ]; then
+if [ $INSTALL_WATCH = true ]; then
+    log "3.1.开始创建容器并执行"
     docker run -d \
     --name watchtower \
     -v /var/run/docker.sock:/var/run/docker.sock \
     containrrr/watchtower
 fi
 
-log "5.docker容器已经运行,下面列出所有在运行的容器"
-
+log "4.下面列出所有容器"
 docker ps
 
+sleep 5s
 
-log "6.接下来你可以选择：\n1) 退出[默认]\n2) 立刻执行京豆变动检测脚本进行测试"
-echo -n -e "\e[33m输入您的选择->\e[0m"
-read action
-if [ "$action" == "2" ]; then
-    docker exec $CONTAINER_NAME bash jd  bean_change now
-else
-    log "即将退出脚本！"
-    exit 1
-fi
+#添加脚本作者助力码
+
+
+log "5.安装已经完成。\n现在你可以访问设备的 ip:5678 用户名：admin  密码：adminadmin  来添加cookie，和其他操作。感谢使用！"
